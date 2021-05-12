f68baae9ddc3037177e0636a760a7b410f2649ed
NhatNM <nhatnmofficial@gmail.com>
2021-05-12 21:25:51 +0700

    Updates

---
+++ precache-manifest.f3e81ab3643d65c345394dbc1f69806b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc92bb8dec6c8ba35ed2",
+    "url": "/static-perfume/static/js/0.fc92bb8d.chunk.js"
+  },
+  {
+    "revision": "c3f545d92e7dfe415eb8",
+    "url": "/static-perfume/static/js/1.c3f545d9.chunk.js"
+  },
+  {
+    "revision": "ac1873c6a4b1bd35c44e",
+    "url": "/static-perfume/static/js/2.ac1873c6.chunk.js"
+  },
+  {
+    "revision": "2ccea3f66cc817520ecf",
+    "url": "/static-perfume/static/js/3.2ccea3f6.chunk.js"
+  },
+  {
+    "revision": "07b8140860a673ac68e3",
+    "url": "/static-perfume/static/js/4.07b81408.chunk.js"
+  },
+  {
+    "revision": "f8dc97e3d4a2c84e2706",
+    "url": "/static-perfume/static/js/main.f8dc97e3.chunk.js"
+  },
+  {
+    "revision": "a9c4315c73b34a38b3bf",
+    "url": "/static-perfume/static/js/runtime~main.a9c4315c.js"
+  },
+  {
+    "revision": "c6a51c8f0bfa082edfd0",
+    "url": "/static-perfume/static/css/7.327666c6.chunk.css"
+  },
+  {
+    "revision": "c6a51c8f0bfa082edfd0",
+    "url": "/static-perfume/static/js/7.c6a51c8f.chunk.js"
+  },
+  {
+    "revision": "57d3abc29b72fc48cbd2",
+    "url": "/static-perfume/static/css/8.6d91359f.chunk.css"
+  },
+  {
+    "revision": "57d3abc29b72fc48cbd2",
+    "url": "/static-perfume/static/js/8.57d3abc2.chunk.js"
+  },
+  {
+    "revision": "767b3ee2454970fc3b10",
+    "url": "/static-perfume/static/css/9.9d909b11.chunk.css"
+  },
+  {
+    "revision": "767b3ee2454970fc3b10",
+    "url": "/static-perfume/static/js/9.767b3ee2.chunk.js"
+  },
+  {
+    "revision": "ecb14373c02635ee4936",
+    "url": "/static-perfume/static/js/10.ecb14373.chunk.js"
+  },
+  {
+    "revision": "11fe2ba7191752dd5f7a",
+    "url": "/static-perfume/static/js/11.11fe2ba7.chunk.js"
+  },
+  {
+    "revision": "9816004bdf9d9b27ccd4",
+    "url": "/static-perfume/static/js/12.9816004b.chunk.js"
+  },
+  {
+    "revision": "1d778771d36d0b078c35",
+    "url": "/static-perfume/static/js/13.1d778771.chunk.js"
+  },
+  {
+    "revision": "f89ca4a64fd026b66c5f",
+    "url": "/static-perfume/static/js/14.f89ca4a6.chunk.js"
+  },
+  {
+    "revision": "9aafa2db1fb18270e7335f3db9f952c9",
+    "url": "/static-perfume/index.html"
+  }
+];
